7df062765104ad7f9f23a56d846cd93c11663f01
Prawalika13 <89731738+Prawalika13@users.noreply.github.com>
2021-09-16 20:04:54 +0530

    Add files via upload

---
+++ 3_Implementation/inc/Activity_2.c
@@ -0,0 +1,29 @@
+/**
+ * @file Activity1.c
+ * @author Prawalika(prawalika.1si17ei027@gmail.com)
+ * @brief LCD Display
+ * @version 0.1
+ * @date 2021-09-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+ #ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
+
+#include <avr/io.h>			/* Include AVR std. library file */
+#include <util/delay.h>			/* Include Delay header file */
+
+#define LCD_Dir  DDRB			/* Define LCD data port direction */
+#define LCD_Port PORTB			/* Define LCD data port */
+#define RS PB2				/* Define Register Select pin */
+#define EN PB3 				/* Define Enable signal pin */
+
+void LCD_Command( unsigned char cmnd );
+void LCD_Char( unsigned char data );
+void lcd_initialization (void);
+void LCD_String (char *str);
+void LCD_String_xy (char row, char pos, char *str);
+void lcd_off();
+
+#endif // ACTIVITY2_H_INCLUDED
